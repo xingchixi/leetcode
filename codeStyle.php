7ee4b38aa07e4700ccc4abd75f8bdb5cc5067e3c
fred <fred@ifonly.com>
2020-04-15 17:22:55 -0700

    codeStyle

---
+++ codeStyle.php
@@ -0,0 +1,67 @@
+<?php
+
+
+class Foo
+{
+	const DATE_APPROVED = '2012-06-01';
+
+	public function sampleMethod($a, $b = null)
+    {
+    }	
+
+    public function aVeryLongMethodName(
+        ClassTypeHint $arg1,
+        &$arg2,
+        array $arg3 = []
+    ) {
+        // method body
+    }
+}
+
+function foo()
+{
+
+	if ($a === $b) {
+        bar();
+    } elseif ($a > $b) {
+        $foo->bar($arg1);
+    } elseif (! function_exists('bar')){
+
+	} else {
+
+	}
+
+	for ($i = 0; $i < 10; $i++) {
+
+	}
+
+	foreach ($iterable as $key => $value) {
+
+	}
+
+	try {
+
+	} catch (Exception $e) {
+
+	} 
+
+	switch ($expr) {
+	    case 0:
+	        echo 'First case, with a break';
+	        break;
+	    default:
+	        echo 'Default case';
+	        break;
+	}
+
+
+
+	while ($expr) {
+
+	}
+
+	do {
+
+	} while ($expr);
+    
+}
