997e9ee298acfcba38f05fe5322c00e1c05a3df0
fred <fred@ifonly.com>
2020-04-16 12:58:11 -0700

    unit test setup

---
+++ reverseKGroup.php
@@ -0,0 +1,171 @@
+<?php
+
+require_once('./ListNode.php');
+
+
+function reverseKGroup($list, $k)
+{
+	if ($list === null || $k <= 1){
+		return $list;
+	}	
+	$firstNode = null;
+	$currentNode = $list;
+	$previousLast = null;
+	while (true){
+		$nodes = [];
+		for ($i = 0; $i < $k; $i++){
+			if ($currentNode !== null){
+				$nodes[] = $currentNode;
+				$currentNode = $currentNode->next;
+			}
+		}
+		if (count($nodes) == $k){
+			if($firstNode === null){
+				$firstNode = $nodes[$k-1];
+			}
+			$nodes[0]->next = $nodes[$k-1]->next;
+			for ($i = 1; $i < $k; $i++){
+				$nodes[$i]->next = $nodes[$i-1];
+			}
+			if ($previousLast !== null){
+				$previousLast->next = $nodes[$k-1];
+			}
+			$previousLast = $nodes[0];
+			$currentNode = $nodes[0]->next;
+		}
+		else{
+			if ($firstNode === null){
+				$firstNode = $nodes[0];
+			}
+			return $firstNode;
+		}
+	}
+	return $firstNode;	
+}
+
+
+
+$array = [];
+for($i=1; $i<=22; $i++){
+	$array[] = $i;
+}
+$list = generateList($array);;
+echo "list: \n";
+echo getStrForList($list). "\n";
+
+$reversed = reverseKGroup($list, 4);
+echo "final reversed: \n";
+echo getStrForList($reversed). "\n";
+
+
+
+
+
+/*
+function reverseKGroup($list, $k){
+	if($list===null || $k <=1){
+		return $list;
+	}
+
+	
+	$firstNode = null;
+	$currentNode = $list;
+	$previousLast = null;
+
+	$count = 0;
+	while (true){
+		$nodes = [];
+		for ($i=0; $i<$k; $i++){
+			if ($currentNode !== null){
+				$nodes[] = $currentNode;
+				//if ($currentNode->next !== null){
+					$currentNode = $currentNode->next;
+				//}
+				//else{
+				//	break;
+				//}
+			}
+			//else{
+			//	break;
+			//}
+
+		}
+
+		for($i=0; $i<count($nodes); $i++){
+			echo "node:". $nodes[$i]->val . " ";
+		}
+		echo "\n";
+
+
+
+		
+		if(count($nodes)==$k){
+			if($firstNode===null){
+				$firstNode = $nodes[$k-1];
+			}
+			$groupNext = $nodes[$k-1]->next;
+
+			for($i=1; $i<$k; $i++){
+				$nodes[$i]->next = $nodes[$i-1];
+			}
+
+			$nodes[0]->next = $groupNext;
+			echo "reversed: " . getStrForList($nodes[$k-1]). "\n";
+
+
+			if($previousLast!==null){
+				echo "previous last " . $previousLast->val . "\n";
+				$previousLast->next = $nodes[$k-1];
+				echo "previous last " . $previousLast->val . " > " .$nodes[$k-1]->val ."\n";
+			}
+			else{
+				echo "no previousLast \n";
+			}
+			
+			$previousLast = $nodes[0];
+			$currentNode = $groupNext;
+		}
+		else{
+			if($firstNode===null){
+				$firstNode = $nodes[0];
+			}
+			return $firstNode;
+		}
+
+
+		
+		$count++;
+		echo "\n";
+
+	}
+
+	return $firstNode;
+	
+
+}
+
+
+
+
+function reverseKGroup2($head, $k) {
+	$dummy=new ListNode(-1);
+    $pre=$cur=$dummy;
+    $dummy->next=$head;
+    $num=0;
+    while($cur=$cur->next) ++$num;
+    while($num>=$k){
+        $cur=$pre->next;  
+        for($i=1;$i<$k;++$i){
+            $t=$cur->next; 
+            $cur->next=$t->next; 
+            $t->next=$pre->next; 
+            $pre->next=$t;
+        }
+        $pre=$cur;
+        $num -=$k;
+    }
+    return $dummy->next;
+    
+}
+
+*/
--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -4,21 +4,22 @@ use PHPUnit\Framework\TestCase;
 include_once 'nParentheses.php';                //relative to current running directory (where we have phpunit)
 
 //https://phpunit.de/getting-started/phpunit-9.html
+// assert functions: https://phpunit.de/manual/6.5/en/appendixes.assertions.html#appendixes.assertions.assertContains
+/*
+set up:
+at root directory create tests directory.  put SampleTest.php in tests
+at root directory:
+wget -O phpunit https://phar.phpunit.de/phpunit-9.phar
+chmod +x phpunit
+./phpunit --version                      // test if it works
 
-//set up:
-//at root directory create tests directory.  put SampleTest.php in tests
-//at root directory:
-//wget -O phpunit https://phar.phpunit.de/phpunit-9.phar
-//chmod +x phpunit
-// ./phpunit --version                      // test if it works
-
-//actual test: 
-// ./phpunit  ./tests                       // test all testCases in tests directory
-// ./phpunit tests/nParenthesesTest.php     // run only one testCase
+actual test: 
+./phpunit  ./tests                       // test all testCases in tests directory
+./phpunit tests/nParenthesesTest.php     // run only one testCase
+*/
 
 final class SampleTest extends TestCase
 {
-
     public function testCanBeUsedAsString(): void
     {
         $a = 1;
